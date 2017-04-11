3a448bdf7ee676a8e8ad3580316deba3bf067243
blackknight75 <devbox4714@gmail.com>
2017-04-11 17:38:02 -0600

    add test for login buttons for guest

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'session#destroy', as: 'signout'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 
   resources :stores, only: [:index]
   resources :sessions, only: [:create, :destroy]
