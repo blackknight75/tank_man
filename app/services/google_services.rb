84e58ff51284839885d316045808dffa1d6b0172
blackknight75 <devbox4714@gmail.com>
2017-04-11 20:00:29 -0600

    add additional store details to index page

--- app/services/google_services.rb
+++ app/services/google_services.rb
@@ -2,6 +2,11 @@ class GoogleServices
 
   def find_near_by_stores
     stores_raw = Faraday.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.615210,-104.758469&radius=16093.4&type=pet_store&keyword=fish&key=#{ENV['google_key']}")
-    parsed_stores = JSON.parse(stores_raw.body, symbolize_names: true)[:results]
+    JSON.parse(stores_raw.body, symbolize_names: true)[:results]
+  end
+
+  def store_details(place_id)
+    raw_store_details = Faraday.get("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{ENV['google_key']}")
+    JSON.parse(raw_store_details.body, symbolize_names: true)[:result]
   end
 end
