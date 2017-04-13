e3f74697df479317306747b8883e5c4bd65ad046
blackknight75 <devbox4714@gmail.com>
2017-04-12 19:13:50 -0600

    add testing for user seeing logout button

---
+++ spec/features/users/logged_in_user_cannot_see_login_button_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'when a user is logged in' do
+  it 'they cannot see the login wiht google button in navbar' do
+    user = User.new(name: "bob", image_url: "https://pbs.twimg.com/profile_images/2577061185/k5z4q8xqcbwq5zk023v0.png")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit root_path
+    within('#navbar') do
+      expect(page).to have_content('Logout')
+      expect(page).to_not have_button('Sign in with Google')
+    end
+  end
+end
