f937e6493beb4d63e78dd310f10beddf48d4be66
blackknight75 <devbox4714@gmail.com>
2017-04-16 17:35:11 -0600

    fix merge errors

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -6,8 +6,8 @@ describe Store do
     it "returns array of stores" do
       VCR.use_cassette("google_stores_array", :record => :new_episodes) do
         expect(Store).to respond_to(:near_by_stores)
-        expect(Store.near_by_stores).to be_a(Array)
-        expect(Store.near_by_stores.length).to eq(11)
+        expect(Store.near_by_stores('162.216.46.154')).to be_a(Array)
+        expect(Store.near_by_stores('162.216.46.154').length).to eq(11)
       end
     end
   end
