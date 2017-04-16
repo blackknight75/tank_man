f139ed9588e097c2b5ce3ae7b47da99748a78272
Spencer Carter <scmountain@gmail.com>
2017-04-16 16:11:34 -0600

    adds test for user dashboard button working

---
+++ spec/features/users/user_can_view_their_own_dashboard_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+
+describe "user can" do
+  before(:each) do
+    @current_user = User.create(name: "scmountain17", oauth_token: ENV['sc_gauth'], image_url: "https://www.native-instruments.com/fileadmin/userlib/images/2452044_6752.f5d784aa1eabbde15ba5e2d90c3ba828.jpg")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@current_user)
+
+    @current_user.tanks.create(name: "Biggin", size: 125, style: "freshwater", description: "overflow wet/dry filter")
+    @current_user.tanks.create(name: "Saltydog", size: 55, style: "Salt", description: "2 blizzard clowns")
+    @current_user.tanks.create(name: "QT", size: 10, style: "coral", description: "not running yet")
+  end
+
+  it "got to dashboard from root" do
+    visit '/'
+    expect(page).to have_content("scmountain17")
+
+    within ('#dropdown1')do
+      click_on "Dashboard"
+    end
+
+    expect(current_path).to eq("/dashboard")
+    expect(page).to have_content("scmountain17")
+    expect(page).to have_content("Biggin")
+    expect(page).to have_content("Saltydog")
+    expect(page).to have_content("QT")
+    expect(page).to have_content("scmountain17")
+  end
+end
