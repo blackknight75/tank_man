588dd8bef407f94ac51d151d79d0a1af084d366f
Spencer Carter <scmountain@gmail.com>
2017-04-19 12:35:46 -0600

    starting model testing

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ group :test do
   gem 'vcr'
   gem 'capybara'
   gem 'webmock'
+  gem 'shoulda-matchers'
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -11,4 +11,14 @@ describe Store do
       end
     end
   end
+  context '.get_yelp_rating' do
+    it "returns one store" do
+      VCR.use_cassette("store") do
+        require "pry"; binding.pry
+        expect(Store.get_yelp_rating(+14698286890)).to be_a(Integer)
+        expect(Store).to respond_to(:store_details)
+
+      end
+    end
+  end
 end
