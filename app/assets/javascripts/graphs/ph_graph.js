3c1187ddc68d4f68da0eb35c12d014254e7b88b6
blackknight75 <devbox4714@gmail.com>
2017-07-10 09:15:11 -0600

    chart populating with tank data. need to format date still

--- app/assets/javascripts/graphs/ph_graph.js
+++ app/assets/javascripts/graphs/ph_graph.js
@@ -1,23 +1,48 @@
-$(function () {
- Highcharts.chart('ph-graph', {
-   chart: {
-       type: 'line'
-   },
-   title: {
-       text: 'Water Parameters'
-   },
-   xAxis: {
-       categories: ['Apples', 'Bananas', (new Date).getDate()]
-   },
-   yAxis: {
-       title: {
-           text: 'Measurement'
-       }
+const host = "http://localhost:3000";
+const ph = []
+const date = []
 
-   },
-   series: [{
-       name: 'pH',
-       data: [7.9, 8.2, 8.1]
-   }]
- });
-});
+function populatePhGraph(){
+  $(function () {
+     Highcharts.chart('ph-graph', {
+       chart: {
+           type: 'line'
+       },
+       title: {
+           text: 'Water Parameters'
+       },
+       xAxis: {
+           categories: date
+       },
+       yAxis: {
+           title: {
+               text: 'Measurement'
+           }
+
+       },
+       series: [{
+           name: 'pH',
+           data: ph
+       }]
+     });
+  });
+}
+function getPh() {
+  $.ajax({
+    method: "GET",
+    url: `${host}/api/v1/tanks/1/recent-ph?tank_id=1`,
+    success:  function(data){
+      populatePhReadings(data)
+    }
+  });
+};
+
+function populatePhReadings(data){
+  data.forEach(function(reading){
+    ph.push(reading.ph)
+    date.push(reading.created_at)
+    populatePhGraph()
+  })
+};
+
+  getPh()
